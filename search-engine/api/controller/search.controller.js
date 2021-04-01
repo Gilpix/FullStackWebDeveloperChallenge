b989e902c11b5352179ac139f9f9bce1cbe80d45
Gilpix <kulsinggh@gmail.com>
2021-04-01 16:37:26 -0400

    moved hemingway.txt to public/corpus folder and used fs to read its content in search.controller

--- search-engine/api/controller/search.controller.js
+++ search-engine/api/controller/search.controller.js
@@ -1,9 +1,16 @@
 "use strict";
 var path = require("path");
 const e = require("express");
+const fetch = require("node-fetch");
+var fs = require('fs');
 
 
 exports.searchAll = (req, res) => {
+
+    const text = fs.readFileSync(path.resolve(__dirname, "../public/corpus/hemingway.txt")).toString()
+
+    console.log("Testing File Reading :  " + text);
+
 
     res.send('Success')
 
