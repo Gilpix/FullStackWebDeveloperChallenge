3ba0a10e2b77f0754554e7f666a4f212c46ecbd3
Gilpix <kulsinggh@gmail.com>
2021-04-01 16:10:45 -0400

    Added search route

--- search-engine/api/routes/index.js
+++ search-engine/api/routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function (req, res, next) {
   res.redirect('http://localhost:3000/');
 });
 
-
+//Search route 
+router.get('/search', function (req, res, next) {
+  res.send('testing search route');
+});
 
 module.exports = router;
